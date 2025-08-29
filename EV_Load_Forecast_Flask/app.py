9c1dcd356842368f9348641cb1ca13c51ab42302
swaroop dhage <swaroop.dhage@gmail.com>
2025-08-29 17:03:24 +0530

    All project files added

---
+++ EV_Load_Forecast_Flask/app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9a93d9628cc06cff2fc95d0da0da4f72168b5ce9f3f31308f9512941ca64e9ad
+size 3163
---
+++ EV_Load_Forecasting_And_Recommendation_System.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:048cd54e22a06b49ed0da164027373f6c95c28b0aae919ba3ad298160a9e0ae1
+size 13196
